adacfea6ef471cd12cac5ca7aac87f39fa27f1d5
Stijntje88 <Stijnvandaelen@outlook.com>
2025-10-21 10:09:06 +0200

    create new database connectie
    +app.config.example
    +connectie string
    +optie voor model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace BarrocIntensTEST.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        
+        //public DbSet<Medewerker> Medewerkers { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseMySql(
+        //        "server=localhost;user=root;password=;database=BarrocIntensTESTDatabase",
+        //        ServerVersion.Parse("8.0.30")
+        //    );
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConfigurationManager.ConnectionStrings["BarrocIntensDatabase"].ConnectionString,ServerVersion.Parse("8.0.30"));
+        }
+        
+
+    }
+}
