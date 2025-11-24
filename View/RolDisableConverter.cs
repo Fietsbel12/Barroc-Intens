fb12b7a9ea39481c7fce7971d71c58faeb16e17b
Bowieman12 <Larsschipperen@gmail.com>
2025-11-24 10:28:44 +0100

    Eigenaar Rol buttons(edit & delete) invisible in adminpanel

---
+++ View/RolDisableConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace BarrocIntens.View
+{
+    public class RolDisableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string rol = value as string;
+            string target = parameter as string;
+
+            // Als rol gelijk is aan de target ("Eigenaar") → verberg de knop
+            return rol == target ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
