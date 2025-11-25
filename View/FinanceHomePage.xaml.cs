ec41d41a058aa84f7c3638243fa82e783a4ef428
Fietsbel12 <Lotusk20@outlook.com>
2025-11-25 13:22:14 +0100

    PDF generate added + styling

--- View/FinanceHomePage.xaml.cs
+++ View/FinanceHomePage.xaml.cs
@@ -8,6 +8,11 @@ using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using PdfSharp.UniversalAccessibility.Drawing;
+using System.IO;
+using PdfSharp.Quality;
 
 namespace BarrocIntens.View
 {
@@ -94,6 +99,76 @@ namespace BarrocIntens.View
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
+        }
+
+        // Offerte buttton
+        private void offerteAanmaken_Click(object sender, RoutedEventArgs e)
+        {
+            var document = new PdfDocument();
+            document.Info.Title = "Offerte";
+
+            var page = document.AddPage();
+
+            var logoPath = Path.Combine(AppContext.BaseDirectory, "Assets", "BarrocIntensHomePageImage.png");
+            using var BarrocIntensHomePageImage = XImage.FromFile(logoPath);
+
+            var gfx = XGraphics.FromPdfPage(page);
+
+            // Fonts
+            var titleFont = new XFont("Arial", 22, XFontStyleEx.Bold);
+            var headerFont = new XFont("Arial", 14, XFontStyleEx.Bold);
+            var regularFont = new XFont("Arial", 12);
+            var smallFont = new XFont("Arial", 10);
+
+            // Kleuren
+            var geel = XBrushes.Gold;
+            var zwart = new XSolidBrush(XColor.FromArgb(0x22, 0x20, 0x1D));
+            var donker = XBrushes.Black;
+
+            // Header
+            gfx.DrawRectangle(geel, 0, 0, page.Width, 80);
+
+            // Logo
+            gfx.DrawImage(BarrocIntensHomePageImage, 30,20,100,40);
+
+            // Titel rechtsboven
+            gfx.DrawString("Offerte", titleFont, zwart,
+                new XPoint(page.Width - 40, 45),
+                XStringFormats.TopRight);
+
+            // Datum
+            gfx.DrawString($"Datum: {DateTime.Now:dd-MM-yyyy}",
+                regularFont, donker, new XPoint(40, 120));
+
+            // Beschrijving
+            gfx.DrawString("Beschrijving", headerFont, donker, new XPoint(40, 170));
+
+            // Lijn onder sectie
+            gfx.DrawLine(new XPen(XColors.Gray, 1),
+                new XPoint(40, 185), new XPoint(page.Width - 40, 185));
+
+            // Inhoud
+            gfx.DrawString("• Koffiezetapparaat", regularFont, donker, new XPoint(60, 210));
+            gfx.DrawString("• Service en installatie inbegrepen", regularFont, donker, new XPoint(60, 230));
+
+            // Footer
+            gfx.DrawString("Barroc Intens",
+                smallFont, XBrushes.Gray, new XPoint(40, page.Height - 40));
+
+            var filename = Path.Combine(Path.GetTempPath(), $"Offerte_{DateTime.Now:yyyyMMddHHmmss}.pdf");
+            document.Save(filename);
+
+            PdfFileUtility.ShowDocument(filename);
+        }
+
+        private void factuurAanmaken_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void contractAanmaken_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
