337cb9d912cb899aee9dae36cf5f84b1716cb2ae
Stijntje88 <Stijnvandaelen@outlook.com>
2025-12-02 10:31:46 +0100

    making taken page
    add listview
    add seeder

--- View/PlannerHomepage.xaml.cs
+++ View/PlannerHomepage.xaml.cs
@@ -1,44 +1,75 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using BarrocIntens.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarrocIntens.View
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PlannerHomepage : Page
     {
         private string medewerkerRol;
+
+        // Takenlijst voor de agenda
+        public ObservableCollection<Taken> TakenLijst { get; set; } = new();
+
         public PlannerHomepage()
         {
             InitializeComponent();
         }
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // Waarde opslaan
-            medewerkerRol = e.Parameter as string;
 
-            // TextBlock bijwerken
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // Rol opslaan en tonen
+            medewerkerRol = e.Parameter as string;
             RolTextBlock.Text = $"Huidige rol: {medewerkerRol}";
+
+            // Taken uit database ophalen
+            using (var db = new AppDbContext())
+            {
+                var takenUitDB = await db.Taken.OrderBy(t => t.Tijd).ToListAsync();
+
+                TakenLijst.Clear();
+                foreach (var taak in takenUitDB)
+                {
+                    TakenLijst.Add(taak);
+                }
+            }
+
+            DataContext = this;
         }
+
+        // Klik op taak in de lijst
+        private void TaskList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem is Taken taak)
+            {
+                TaskDetailName.Text = taak.Name;
+                TaskDetailDescription.Text = taak.Description;
+                TaskDetailTime.Text = taak.Tijd.ToString("dd-MM-yyyy HH:mm");
+
+                TaskDetailPanel.Visibility = Visibility.Visible;
+            }
+        }
+
+        // Sluit detailpaneel
+        private void CloseDetail_Click(object sender, RoutedEventArgs e)
+        {
+            TaskDetailPanel.Visibility = Visibility.Collapsed;
+        }
+
+        // Back-knop
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.GoBack();
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
         }
     }
 }
